5150c9c8a9f252d29ae758579bf3cea850344c29
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-26 18:18:31 +0100

    Brew formula update for numary version 1.0.0-rc.3

--- Formula/numary.rb
+++ Formula/numary.rb
@@ -5,20 +5,20 @@
 class Numary < Formula
   desc ""
   homepage "https://numary.com"
-  version "1.0.0-rc.2"
+  version "1.0.0-rc.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/numary/ledger/releases/download/v1.0.0-rc.2/numary_1.0.0-rc.2_macOS-ARM64.tar.gz"
-      sha256 "3dfab15c2cba1133c90450f1c4f834ad3a9f43b223f55ac451c81ed2705ec7d5"
+      url "https://github.com/numary/ledger/releases/download/1.0.0-rc.3/numary_1.0.0-rc.3_macOS-ARM64.tar.gz"
+      sha256 "4155e9ea13f2177a2e024cb331303cd337dabe2c3442cb93a70854ca256dd7fe"
 
       def install
         bin.install "numary"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/numary/ledger/releases/download/v1.0.0-rc.2/numary_1.0.0-rc.2_macOS-64bit.tar.gz"
-      sha256 "7ccab8a7fede6e74a973b43a3d62c95a002177c2421dbd0a2fbadb43c4258c97"
+      url "https://github.com/numary/ledger/releases/download/1.0.0-rc.3/numary_1.0.0-rc.3_macOS-64bit.tar.gz"
+      sha256 "cf6ab1dc8b361ee8e331a7255b9953b22757049180ed7b00f4c315c3a6ba3442"
 
       def install
         bin.install "numary"
@@ -28,8 +28,8 @@ class Numary < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/numary/ledger/releases/download/v1.0.0-rc.2/numary_1.0.0-rc.2_Linux-64bit.tar.gz"
-      sha256 "057defa349b51cfa41ef2a1e89ff424fa657befd666212f01ba90b729cff60c2"
+      url "https://github.com/numary/ledger/releases/download/1.0.0-rc.3/numary_1.0.0-rc.3_Linux-64bit.tar.gz"
+      sha256 "45ad26ed818144b520ccad485c1e2fb75e8fedb754a3d8f1d1101a554359312b"
 
       def install
         bin.install "numary"
